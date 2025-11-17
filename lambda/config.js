817b385a51ff1e6eb0d4e1b2355c4fa6601585e4
dom209 <dominic.reid@dmar.me.uk>
2025-11-17 17:42:02 +0000

    Update config.js
    update config

--- lambda/config.js
+++ lambda/config.js
@@ -1,7 +1,7 @@
 // config.js
 module.exports = {
-    ABS_API_KEY: 'xxxxxx',
-    SERVER_URL: 'https://abs.domain.tld',
+    ABS_API_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJrZXlJZCI6ImRhYWFlMTZmLTE3N2QtNDljZi1hZjhhLWZhZWRlNjA4ZDlkMCIsIm5hbWUiOiJhbGV4YSIsInR5cGUiOiJhcGkiLCJpYXQiOjE3NjM0MDExMDV9.5YnFiauuLEOn6wg4l36UdDcG7SsnbbPEE-wwG3ag5yg',
+    SERVER_URL: 'https://audiobookshelf.dominicreid.uk/',
     USER_AGENT: 'AlexaSkill'
     // OTHER OPTIONAL HEADERS
     // CFAccessClientId: 'xxxxxxxxx.access',
